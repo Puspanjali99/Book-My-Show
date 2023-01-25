03c7711200e53d1a9dd3727780bcf73e2c6c045b
Puspanjali <puspanjalipriyadarshini9@email.com>
2023-01-25 17:57:48 +0530

    BOOK MY SHOW CODE ADDED

---
+++ src/main/java/com/example/Book_my_show_backend/Controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.Book_my_show_backend.Controller;
+
+import com.example.Book_my_show_backend.Dtos.UserRequestDto;
+import com.example.Book_my_show_backend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/add")
+    public String addUser(@RequestBody UserRequestDto userRequestDto){
+
+        return userService.createUser(userRequestDto);
+
+    }
+
+    //Find Userby Name h.w
+
+
+    //Find all Users
+
+}
